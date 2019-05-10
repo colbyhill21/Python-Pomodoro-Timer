4e6c5e0376c82deb96ecf7c33135b04a81e6b54c
Colby Hill <colbyhill21@yahoo.com>
2019-05-10 17:00:25 -0400

    Added the pause button functionality in, appears to have no issues.

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 # import pygame
 # pygame.init()
 
@@ -15,11 +16,14 @@ rest_txt = 'Rest Session {x}'
 # TODO make GUI look a bit better
 
 
-def pause_pressed(pbt):
+def pause_pressed(pbt, _timer):
+
     if pbt.get() == "Pause":
         pbt.set("Resume")
+        _timer.pause()
     else:
         pbt.set("Pause")
+        _timer.resume()
 
 
 class Timer:
@@ -31,6 +35,7 @@ class Timer:
     total_seconds = 0
     focus_length = 21
     rest_length = 4
+    is_paused = False
 
     def __init__(self, parent):
         # label displaying time
@@ -55,18 +60,19 @@ class Timer:
         self.time_label.after(1000, self.focus_session_loop)  # start the focus loop after 1s
 
     def focus_session_loop(self):
-        self.seconds -= 1
-        if self.seconds < 0:
-            self.minutes -= 1
-            self.seconds = 59
-            if self.minutes < 0:
-                # focus session has ended
-                self.num_focus += 1
-                self.time_label.after(0, self.start_rest_session)  # start a rest session
+        if not self.is_paused:
+            self.seconds -= 1
+            if self.seconds < 0:
+                self.minutes -= 1
+                self.seconds = 59
+                if self.minutes < 0:
+                    # focus session has ended
+                    self.num_focus += 1
+                    self.time_label.after(0, self.start_rest_session)  # start a rest session
 
-        # display the new time
-        self.time_label.configure(text=t_txt.format(m=self.minutes, s=self.seconds))
-        self.time_label.after(1000, self.focus_session_loop)  # refresh after 1s
+            # display the new time
+            self.time_label.configure(text=t_txt.format(m=self.minutes, s=self.seconds))
+            self.time_label.after(1000, self.focus_session_loop)  # refresh after 1s
 
     def start_rest_session(self):
         # setup in order to run a focus session.
@@ -83,33 +89,44 @@ class Timer:
         self.time_label.after(1000, self.rest_session_loop)  # start the rest loop
 
     def rest_session_loop(self):
-        self.seconds -= 1
-        if self.seconds < 0:
-            self.seconds = 59
-            self.minutes -= 1
-            if self.minutes < 0:
-                # once rest session ends, start another focus session
-                self.num_rest += 1
-                self.time_label.after(0, self.start_focus_session)
+        if not self.is_paused:
+            self.seconds -= 1
+            if self.seconds < 0:
+                self.seconds = 59
+                self.minutes -= 1
+                if self.minutes < 0:
+                    # once rest session ends, start another focus session
+                    self.num_rest += 1
+                    self.time_label.after(0, self.start_focus_session)
 
-        # display the new time
-        self.time_label.configure(text=t_txt.format(m=self.minutes, s=self.seconds))
-        self.time_label.after(1000, self.rest_session_loop)  # refresh after 1s
+            # display the new time
+            self.time_label.configure(text=t_txt.format(m=self.minutes, s=self.seconds))
+            self.time_label.after(1000, self.rest_session_loop)  # refresh after 1s
+
+    def pause(self):
+        self.is_paused = True
+
+    def resume(self):
+        self.is_paused = False
+
+        # now we need to push back into the loop.
+        if self.num_focus > self.num_rest:  # this means that we are currently in a rest session
+            self.time_label.after(1000, self.rest_session_loop)
+        else:
+            self.time_label.after(1000, self.focus_session_loop)
 
 
 # Below here is the "main"
 
 # initialize objects
+welcome_label = tk.Label(window, text='Welcome to Focus Timer')
+welcome_label.pack()
+timer = Timer(window)
 pause_button_text = tk.StringVar()
 pause_button_text.set("Pause")
 pause_button = tk.Button(window, textvariable=pause_button_text, width=30,
-                         command=lambda: pause_pressed(pause_button_text))
+                         command=lambda: pause_pressed(pause_button_text, timer))
 
-welcome_label = tk.Label(window, text='Welcome to Focus Timer')
-
-# organize objects onto display
-welcome_label.pack()
-timer = Timer(window)
 pause_button.pack()
 
 # run the program?
